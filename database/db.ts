1ddc01fc37a2be342aecfcb92b64156f698e8203
Aek <aekburut5740@gmail.com github>
2026-05-27 12:00:59 +0700

    pushdata

--- database/db.ts
+++ database/db.ts
@@ -1,1 +1,11 @@
-export { default as prisma } from '../src/lib/prisma'
+import { Client } from "pg"
+
+export const db = new Client({
+  user:     process.env.DB_USER,
+  host:     process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port:     Number(process.env.DB_PORT),
+})
+
+db.connect()
